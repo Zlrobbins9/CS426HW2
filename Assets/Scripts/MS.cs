506da790c7ac9fcda14a3b997273c24fabae9b00
Zlrobbins9 <55512911+Zlrobbins9@users.noreply.github.com>
2024-02-06 14:06:35 -0600

    Added keyboard, made large mouse to platform on

--- Assets/Scripts/MS.cs
+++ Assets/Scripts/MS.cs
@@ -6,9 +6,11 @@ using UnityEngine;
 public class MS : MonoBehaviour
 {
     public bool Dropped = false;
+    public Material Material1;
     // Start is called before the first frame update
     void Start()
     {
+        gameObject.transform.GetChild(1).GetComponent<SkinnedMeshRenderer>().material = Material1;
         Debug.Log(this.gameObject.tag);
     }
 
