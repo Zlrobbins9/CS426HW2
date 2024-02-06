feeebd7c04910f1cce5445f9e2daba1fcdfaa428
Kevin Cheung <kevin_cheung20@yahoo.com>
2024-02-06 16:23:21 -0600

    added score and material matching

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -6,17 +6,49 @@ using Unity.Netcode;
 
 public class Target : NetworkBehaviour
 {
-
-    //this method is called whenever a collision is detected
+    public Score scoreManager;
     private void OnCollisionEnter(Collision collision)
     {
+        // Check if this GameObject has any children
+        if (transform.childCount > 0)
+        {
+            // Get the Renderer component of the 2nd child
+            Renderer mouse = transform.GetChild(1).GetComponent<Renderer>();
 
-        // printing if collision is detected on the console
-        Debug.Log("Collision Detected");
+            // Get the Renderer component of the colliding GameObject (Object B)
+            Renderer target = collision.gameObject.GetComponent<Renderer>();
 
-        // if the collision is detected destroy the object
-        DestroyTargetServerRpc();
+            // Make sure both Renderers are found
+            if (mouse != null && target != null)
+            {
+                // Compare the materials
+                if (mouse.material.name == target.material.name)
+                {
+                    Debug.Log("Materials match!");
+                    scoreManager.AddPoint();
+                    DestroyTargetServerRpc();
+                    // Add any additional logic here for when materials match
+                }
+                else
+                {
+                    Debug.Log("Materials do not match.");
+                    // Add any additional logic here for when materials do not match
+                }
+            }
+            else
+            {
+                // One or both of the Renderers were not found
+                Debug.LogError("One or both objects do not have a Renderer component with a material.");
+            }
+        }
+        else
+        {
+            // This GameObject does not have children
+            Debug.LogError("This GameObject has no children to compare materials with.");
+        }
     }
+
+
 
     // client can not spawn or destroy objects
     // so we need to use ServerRpc
